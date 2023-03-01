33a5db57f858b969c53953d4d97c3a0d87ab0c69
hectorpaez17 <a20200419@utem.edu.mx>
2023-02-28 20:29:32 -0600

    Se arreglo el error con el responsive del navbar

--- js/script.js
+++ js/script.js
@@ -5,3 +5,15 @@ menu.onclick = () => {
     menu.classList.toggle('bx-x');
     navbar.classList.toggle('open');
 }
+
+const $btnOcultar = document.querySelector("#menu-icon"),
+	// $btnMostrar = document.querySelector("#cerrar"),
+	$logo = document.querySelector("#lg");
+    
+    // $btnMostrar.addEventListener("click", () => {   
+ 	// $logo.style.display = "block";
+    // });
+
+    $btnOcultar.addEventListener("click", () => {
+    	$logo.style.display = "none";
+    });
